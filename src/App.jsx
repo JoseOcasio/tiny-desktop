9cd02cbac53b890afbfa454f8c45cbc11c033e5a
Jose Ocasio <joseocasio@Joses-MacBook-Air.local>
2022-06-18 10:04:41 -0400

    Added trash component functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const background = 'linear-gradient(90deg, rgba(49,36,239,1) 0%, rgba(67,0,168,1
 function App() {
   return (
     <div className="App">
+      <h2 className='Desktop-header'>Tiny Desktop</h2>
       <div className='Desktop-container'>
         <Desktop/>
       </div>
--- src/components/trash/trash.jsx
+++ src/components/trash/trash.jsx
@@ -1,9 +1,19 @@
+import './trash.css';
 
 function Trash() {
+
+    const filesInTrash = ["Document1", "Music1", "Document2", "Video2", "Application3", "Game2"];
+
+    const files = filesInTrash.map(file => {
+        return <div className='item item-margin'>{file}</div>;
+    })
     return (
-        <div>
-            Trash component
+        <div className='item-container'>
+            <div className="trash-container">
+                {files}
+            </div>
         </div>
+        
     );
 }
 
